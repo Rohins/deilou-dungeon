dbe1b64507009f716752a11a095cdb02df2cd9bb
Ricardo Rodriguez <rodriguez.ric@gmail.com>
2017-04-28 23:20:42 -0400

    Added function resolve to reflect the game's logic. Added a function stub for computing mechanics.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,18 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  /**
+   * State representing the card is complete. When true
+   * the card will be removed.
+   **/
+  public complete: boolean;
+
+  /**
+   * The face value of the card. Legal values 1-9,J,Q,K,A
+   **/
   public faceValue: string;
+
+  /**
+   * State representing if the card is facing up.
+   **/
   public faceUp: boolean;
+
+  /**
+   * The suite of the card: clovers, hearts, diamonds, spades.
+   **/
   public suite: string;
 
   constructor() { }
 
   ngOnInit() {
     this.faceValue = "K";
-    this.faceUp = true;
+    this.faceUp = false;
     this.suite = "clovers";
+    this.complete = false;
   }
 
+  /**
+   * Helper function for determining a numeric value for
+   * the card's face value.
+   **/
   getValue() {
     return 10;
   }
@@ -26,8 +48,33 @@ export class CardComponent implements OnInit {
    * Flip the card!
    **/
   flip() {
-    console.log("Flipping!");
     this.faceUp = this.faceUp ? false : true;
+  }
+
+  /**
+   * If the card is facing down, flip it up.
+   * Otherwise, resolve the card's mechanics.
+   **/
+  resolve() {
+    if (!this.faceUp) {
+      this.flip();
+      return;
+    }
+
+    this.computeMechanics();
+  }
+  
+  /**
+   * Compute the logic based on the card's suite
+   * and face value.
+   **/
+  computeMechanics() {
+    switch(this.suite) {
+      case "clovers":
+        console.log(`Take ${this.getValue()} damage!`);
+        this.complete = true;
+        break;
+    }
   }
 
 }
